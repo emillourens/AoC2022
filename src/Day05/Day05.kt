283792994ec73592bf3cda0bdf30a5e5cf56a0a0
emillourens <emillourens@gmail.com>
2022-12-05 17:29:34 +0000

    final

--- src/Day05/Day05.kt
+++ src/Day05/Day05.kt
@@ -2,127 +2,140 @@
 fun main() {
     fun part1(input: List<String>): String {
 
-        var counter = 0
-
-        var stacks = mutableListOf(mutableListOf<String>())
-
-        for (i in 0..7) {
-            stacks.add(mutableListOf())
-        }
-
-        var stacksstring = "  J  B T\n" +
-                "  ML QLR\n" +
-                "  GQ WSBL\n" +
-                "D DT MGVP\n" +
-                "T NNNDJGN\n" +
-                "WHHSCNRWD\n" +
-                "NPPWHHBNG\n" +
-                "LCWCPTMZW"
-        for (char in stacksstring) {
-            if (char.toString() == "\n") {
-                counter = 0
-            } else {
-                if (char.toString() == " ") {
-                    counter++
-                    continue
-                } else {
-                    stacks[counter].add(char.toString())
-                    counter++
-                }
-            }
-        }
-
-        var reversedStacks = mutableListOf<MutableList<String>>()
-
-        for (stack in stacks) {
-            reversedStacks.add(stack.reversed().toMutableList())
-        }
-
-
-        //var stacks = mutableListOf(mutableListOf("Z", "N"), mutableListOf("M", "C", "C"), mutableListOf("P"))
+        val stacks = mutableMapOf<String, MutableList<String>>()
 
         for (instructions in input) {
-            println(instructions)
-            var cleanedInput = instructions.replace("move ", "").replace(" from ", "").replace(" to ", "")
+            var counter = 1
+            if (instructions != "_________" && !instructions.contains("move")) {
 
-            var amount = cleanedInput[0].toString()
-            var from = cleanedInput[1].toString()
-            var to = cleanedInput[2].toString()
-
-            //var sequence = mutableListOf<String>()
-
-            for (i in 0 until amount.toInt() step 1) {
-                var box = ""
-                when (from) {
-                    "1" -> {
-
-                        box += reversedStacks[0].removeLast()
-                        println("Box to be removed from stack 1 $box")
-                        //sequence.add(box)
-                    }
-
-                    "2" -> {
-                        //var box = ""
-                        box += reversedStacks[1].removeLast()
-                        println("Box to be removed from stack 2 $box")
-                        //sequence.add(box)
-                    }
-
-                    "3" -> {
-                        //var box = ""
-                        box += reversedStacks[2].removeLast()
-                        println("Box to be removed from stack 3 $box")
-                        //sequence.add(box)
+                if (stacks.size < instructions.length) {
+                    for (i in 1..instructions.length + 1) {
+                        stacks[i.toString()] = mutableListOf()
                     }
                 }
 
-                when (to) {
-                    "1" -> {
-                        //var box = ""
-                        //box += sequence.removeFirst()
-                        println("Box to be added to stack 1 $box")
-                        reversedStacks[0].add(box)
+                for (char in instructions) {
+                    if (char.toString() == "\n") {
+                        counter = 1
+                    } else {
+                        if (char.toString() == " ") {
+                            counter++
+                            continue
+                        } else {
+                            stacks[counter.toString()]?.add(char.toString())
+                            counter++
+                        }
                     }
+                }
+            }
+            if(instructions == "_________")
+            {
+                for (i in 1 .. stacks.size)
+                {
+                    stacks[i.toString()] = stacks[i.toString()]?.reversed()?.toMutableList()!!
+                }
+            }
+            if (instructions.contains("move")) {
 
-                    "2" -> {
-                        //var box = ""
-                        //box += sequence.removeFirst()
-                        println("Box to be added to stack 2 $box")
-                        reversedStacks[1].add(box)
-                    }
+                println(instructions)
+                val cleanedInput = instructions.replace("move ", "").replace(" from ", "|").replace(" to ", "|").split("|")
 
-                    "3" -> {
-                        //var box = ""
-                        //box += sequence.removeFirst()
-                        println("Box to be added to stack 3 $box")
-                        reversedStacks[2].add(box)
-                    }
+                val amount = cleanedInput[0]
+                val from = cleanedInput[1]
+                val to = cleanedInput[2]
+
+                for (i in 1 .. amount.toInt()) {
+                    var box: String = stacks[from]?.pop().toString()
+                    println("$box to be removed from stack $from and added to $to ")
+
+                    stacks[to]?.push(box)
                 }
             }
         }
 
         var output = ""
 
-        for (stack in reversedStacks) {
-            output += stack.removeLast()
+        for (i in 1 .. stacks.count() step 1) {
+            output += stacks[i.toString()]?.last()
         }
-        print(output)
-
+        //print(output)
 
         return output
 
     }
 
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): String {
+        val stacks = mutableMapOf<String, MutableList<String>>()
+
+        for (instructions in input) {
+            var counter = 1
+            if (instructions != "_________" && !instructions.contains("move")) {
+
+                if (stacks.size < instructions.length) {
+                    for (i in 1..instructions.length + 1) {
+                        stacks[i.toString()] = mutableListOf()
+                    }
+                }
+
+                for (char in instructions) {
+                    if (char.toString() == "\n") {
+                        counter = 1
+                    } else {
+                        if (char.toString() == " ") {
+                            counter++
+                            continue
+                        } else {
+                            stacks[counter.toString()]?.add(char.toString())
+                            counter++
+                        }
+                    }
+                }
+            }
+            if (instructions == "_________") {
+                for (i in 1..stacks.size) {
+                    stacks[i.toString()] = stacks[i.toString()]?.reversed()?.toMutableList()!!
+                }
+            }
+            if (instructions.contains("move")) {
+
+                println(instructions)
+                val cleanedInput =
+                    instructions.replace("move ", "").replace(" from ", "|").replace(" to ", "|").split("|")
+
+                val amount = cleanedInput[0]
+                val from = cleanedInput[1]
+                val to = cleanedInput[2]
+
+                //var box = M
+                val boxes = stacks[from]?.takeLast(amount.toInt())
+
+                repeat(amount.toInt()){stacks[from]?.removeLastOrNull()}
+                println("$boxes to be removed from stack $from and added to $to ")
+
+                for ( box in boxes!!)
+                {
+                    stacks[to]?.add(box)
+                }
+            }
+        }
+
+        var output = ""
+
+        for (i in 1 .. stacks.count() step 1) {
+            output += stacks[i.toString()]?.last()
+        }
+        //print(output)
+
+        return output
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05/Day05_test")
-    //check(part1(testInput) == "CMZ")
+    check(part1(testInput) == "CMZ")
+    check(part2(testInput) == "MCD")
 
     val input = readInput("Day05/Day05")
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
