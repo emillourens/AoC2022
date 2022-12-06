fb274151ffd73fe8920bb8a5f5ae94ed18f4d4f2
emillourens <emillourens@gmail.com>
2022-12-06 07:47:19 +0000

    Day 07

--- src/Day06/Day06.kt
+++ src/Day06/Day06.kt
@@ -1,17 +1,105 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        var index = 0
+        var last4 = ""
+        for (dataStreams in input)
+        {
+            for (streamChar in dataStreams)
+            {
+
+                if(last4.contains(streamChar))
+                {
+                    last4 += streamChar
+                    if (last4.length > 4)
+                    {
+                        last4=last4.drop(1)
+                    }
+                }
+                else
+                {
+                    if (last4.length == 4)
+                    {
+                        if(last4.allUnique())
+                        {
+                            index = counter
+                            break
+                        }
+                        else
+                        {
+                            last4 += streamChar
+                            last4=last4.drop(1)
+                        }
+                    }
+                    else if (last4.length > 4)
+                    {
+                        last4 += streamChar
+                        last4=last4.drop(1)
+                    }
+                    else
+                    {
+                        last4 += streamChar
+                    }
+                }
+                counter++
+            }
+        }
+        return index
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        var index = 0
+        var last14 = ""
+        for (dataStreams in input)
+        {
+            for (streamChar in dataStreams)
+            {
+                counter++
+                if(last14.contains(streamChar))
+                {
+                    last14 += streamChar
+                    if (last14.length > 14)
+                    {
+                        last14=last14.drop(1)
+                    }
+                }
+                else
+                {
+                    if (last14.length == 14)
+                    {
+                        last14 += streamChar
+                        last14=last14.drop(1)
+                        if(last14.allUnique())
+                        {
+                            index = counter
+                            break
+                        }
+                    }
+                    else if (last14.length > 14)
+                    {
+                        last14 += streamChar
+                        last14=last14.drop(1)
+                    }
+                    else
+                    {
+                        last14 += streamChar
+                    }
+                }
+
+            }
+        }
+        return index
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06/Day06_test")
+    //check(part1(testInput) == 10)
+    check(part2(testInput) == 19)
+
+    val input = readInput("Day06/Day06")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -20,3 +20,5 @@ fun <T> MutableList<T>.push(item: T) = this.add(this.count(), item)
 fun <T> MutableList<T>.pop(): T? = if(this.count() > 0) this.removeAt(this.count() - 1) else null
 fun <T> MutableList<T>.peek(): T? = if(this.count() > 0) this[this.count() - 1] else null
 fun <T> MutableList<T>.hasMore() = this.count() > 0
+
+fun String.allUnique(): Boolean = all(hashSetOf<Char>()::add)
