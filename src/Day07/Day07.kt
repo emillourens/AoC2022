c77c0da7c58c5877948fba6037c0e3bfeb61230d
emillourens <emillourens@gmail.com>
2022-12-09 13:37:33 +0000

    Day 08

--- src/Day07/Day07.kt
+++ src/Day07/Day07.kt
@@ -1,17 +1,100 @@
+data class File(val name: String, val size: Long)
+
+class Directory(val parent: Directory?) {
+    val children = mutableMapOf<String, Directory>()
+    val files = mutableListOf<File>()
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getDirectorySize(directory: Directory): Long {
+        var totalSize = directory.files.sumOf { it.size }
+        for (c in directory.children) {
+            totalSize += getDirectorySize(c.value)
+        }
+        return totalSize
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun getAllChildren(directory: Directory): List<Directory> {
+        return directory.children.values.flatMap { getAllChildren(it) } + directory
+    }
+
+    fun traverseDirectory(directory: Directory, path: String): Directory {
+        val dirs = path.split("/")
+        var currentDir = directory
+        for (d in dirs) {
+            if (d.isNotEmpty())
+                currentDir = currentDir.children[d]!!
+        }
+        return currentDir
+    }
+
+    fun readInput(root: Directory, input: List<String>) {
+        var currentDirectory = root
+
+        var currentLine = 0
+        while (currentLine < input.size) {
+            var line = input[currentLine]
+            if (line.substring(0, 4) == "$ cd") {
+                // Change directories
+                val newDirectory = line.split(" ")[2]
+                if (newDirectory[0] == '/') {
+                    currentDirectory = traverseDirectory(root, newDirectory.substringAfter('/'))
+                } else if (newDirectory == ".." && currentDirectory.parent != null) {
+                    currentDirectory = currentDirectory.parent!!
+                } else {
+                    currentDirectory = traverseDirectory(currentDirectory, newDirectory)
+                }
+                currentLine++
+            } else if (line.substring(0, 4) == "$ ls") {
+                // Add children
+                currentLine++
+                line = input[currentLine]
+                while (currentLine < input.size && line[0] != '$') {
+                    if (line.substring(0, 3) == "dir") {
+                        // Add directory as child
+                        val newDirectory = Directory(currentDirectory)
+                        currentDirectory.children[line.split(" ")[1]] = newDirectory
+                    } else {
+                        // Add file as child
+                        val fileInfo = line.split(" ")
+                        currentDirectory.files.add(File(fileInfo[1], fileInfo[0].toLong()))
+                    }
+                    currentLine++
+                    if (currentLine < input.size) line = input[currentLine]
+                }
+            }
+        }
+    }
+
+    fun part1(input: List<String>): Long {
+        val root = Directory(null)
+        readInput(root, input)
+
+        val directories = getAllChildren(root)
+        return directories.sumOf { it ->
+            val size = getDirectorySize(it)
+            if (size <= 100_000) size else 0
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        val root = Directory(null)
+        readInput(root, input)
+
+        val requiredSpace = 30_000_000 - (70_000_000 - getDirectorySize(root))
+        val directories = getAllChildren(root).map {
+            val size = getDirectorySize(it)
+            if (size >= requiredSpace) size else Long.MAX_VALUE
+        }
+        return directories.minByOrNull { it }!!
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day07/Day07_test")
+    check(part1(testInput) == 95_437L)
+    check(part2(testInput) == 24_933_642L)
 
-    val input = readInput("Day01")
+    val input = readInput("Day07/Day07")
     println(part1(input))
     println(part2(input))
 }
--- src/Day08/Day08.kt
+++ src/Day08/Day08.kt
@@ -1,63 +1,44 @@
 fun main() {
     fun part1(input: List<String>): Int {
 
-        var forest = Array<Array<Int>[input.size]>[input.size]
-        var counter = 0
-        for line in input
-        {
-            forest[counter] = line.toCharArray()
-        }
+        var forest: MutableList<MutableList<Int>> = ArrayList()//Array(input.size) { IntArray(input.size) }
 
         var counter = 0
-        for i in forest.indices
+
+        for ((i, line) in input.withIndex())
         {
-            var largest = forest[i][0]
-            for j in forest[i].indices
-            {
-                if(largest > forest[i][j])
-                {
-                    counter++
-                    largest = tree
-                }
-            }
-        }
-        for i in forest.indices
-        {
-            var largest = forest[0][i]
-            for j in forest[i].indices
-            {
-                if(largest > forest[j][i])
-                {
-                    counter++
-                    largest = tree
-                }
-            }
-        }
-        for i in forest.indices.reversed()
-        {
-            var largest = forest[i][0]
-            for j in forest[i].indices
-            {
-                if(largest > forest[i][j])
-                {
-                    counter++
-                    largest = tree
-                }
-            }
-        }
-        for i in forest.indices.reversed()
-        {
-            var largest = forest[0][i]
-            for j in forest[i].indices.reversed()
-            {
-                if(largest > forest[j][i])
-                {
-                    counter++
-                    largest = tree
-                }
-            }
+            var tempList: MutableList<Int> = ArrayList()
+            for ( j in line.indices)
+                tempList.add(line[j].digitToInt())
+
+            forest.add(tempList)
         }
 
+        var forestTranspose = forest
+        for (i in forest.indices) {
+            for (j in forest[i].indices) {
+                forestTranspose[j][i] = forest[i][j]
+            }
+        }
+        ///var forest2 = forest.zip()
+
+        for (row in forest.indices)
+        {
+            for (column in forest[row].indices)
+            {
+                var tree = forest[row][column]
+                var right = forest[row].subList(0, column)
+                var left = forest[row].subList(column + 1, forest.size)
+                var leftT = forestTranspose[column].subList(0, row)
+                var rightT = forestTranspose[column].subList(row + 1, forest.size)
+
+                if( right.all { x -> x < tree } || left.all {  x -> x < tree  } || leftT.all {  x -> x < tree  } || rightT.all {  x -> x < tree  })
+                    counter++
+
+            }
+            //println("Visible tree $largest")
+        }
+        println(counter)
         return counter
     }
 
@@ -67,10 +48,10 @@ fun main() {
 
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day08/Day08_test")
+    check(part1(testInput) == 21)
 
-    val input = readInput("Day01")
+    val input = readInput("Day08/Day08")
     println(part1(input))
-    println(part2(input))
+    //println(part2(input))
 }
